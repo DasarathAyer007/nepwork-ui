bcb156d7d7f3f479977f3d9644d5311601a56b35
JasmirChy <jasmirchy@gmail.com>
2026-06-12 14:45:22 +0545

    Added Jobs page and updated router, home index, footer

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import MainLayout from '../layouts/User/MainLayout';
 import Home from '../pages/Home';
+import Jobs from '../pages/Jobs';
 import LogIn from '../pages/LogIn';
 import SignUp from '../pages/SignUp';
 
@@ -10,6 +11,7 @@ function route() {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
+        <Route path="jobs" element={<Jobs />} />
       </Route>
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<LogIn />} />
